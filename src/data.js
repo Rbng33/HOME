19269cd45724ae06620308c87ac45b4d1254e070
Rbng33 <128463403+Rbng33@users.noreply.github.com>
2023-04-23 14:13:42 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,81 @@
+
+import one from "./assets/imagecreche1.png";
+import two from "./assets/imagecreche2.png";
+import three from "./assets/imagecreche3.png";
+import kid1 from "./assets/Enfant1.png";
+import kid2 from "./assets/Enfant2.png";
+import kid3 from "./assets/Enfant3.png";
+
+
+
+
+export const CrecheSliderdata = [ 
+
+    { 
+        image : one ,
+        name : 'Creche1' , 
+        Location : 'Kouba, Alger.' ,
+        Description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat eget mauris ut convallis. Ut tincidunt justo quis nunc tincidunt tincidunt. ',
+    } ,
+    { 
+        image : two ,
+        name : 'Creche2' , 
+        Location : 'Baba Hssen, Alger.' ,
+        Description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat eget mauris ut convallis. Ut tincidunt justo quis nunc tincidunt tincidunt. ',
+    },
+    { 
+        image : three ,
+        name : 'Creche3' , 
+        Location : 'Gdyel, Oran.' ,
+        Description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat eget mauris ut convallis. Ut tincidunt justo quis nunc tincidunt tincidunt. ',
+    },
+    { 
+        image : one ,
+        name : 'Creche ta3 ryad' , 
+        Location : 'Hydra, Alger.' ,
+        Description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat eget mauris ut convallis. Ut tincidunt justo quis nunc tincidunt tincidunt. ',
+    },
+]
+
+
+
+
+
+
+
+
+
+export const EnfantSliderdata = [ 
+
+    { 
+        image : kid1 ,
+        name : 'Ines' , 
+        Age : '4' ,
+      
+    } ,
+    { 
+        image : kid2 ,
+        name : 'Riyad' , 
+        Age : '3' ,
+      
+    },
+    { 
+        image : kid3 ,
+        name : 'Wassim' , 
+        Age : '5' ,
+       
+    },
+    { 
+        image : kid1,
+        name : 'Cerine' , 
+        Age : '3' ,
+        
+    },
+]
+
+
+
+
+
+
+
